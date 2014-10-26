9b3119367cb48f220fdd173c91e29df76837e250
Kyle Fleming <garthex@gmail.com>
2014-10-25 19:00:44 -0700

    Initial Commit

---
+++ Minitrees/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Minitrees
+//
+//  Created by Kyle Fleming on 10/25/14.
+//  Copyright (c) 2014 Kyle Fleming. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
